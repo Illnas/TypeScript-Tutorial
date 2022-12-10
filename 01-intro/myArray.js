ecce32295f040eabd187d800a54384c1b3a35120
Nadatari <dariosting@gmail.com>
2022-12-10 15:09:06 +0100

    Finished with arrays

---
+++ 01-intro/myArray.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var superHeroes = [];
+/* const heroPower: number[] = [] */
+var heroPower = [];
+var allusers = [];
+//array inside array with type number
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeroes.push("Spiderman");
+heroPower.push(2);
+allusers.push({ name: "h", isActive: true });
---
+++ 01-intro/myArray.ts
@@ -0,0 +1,24 @@
+
+const superHeroes: string[] = []
+/* const heroPower: number[] = [] */
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allusers: User[] = []
+
+//array inside array with type number
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+superHeroes.push("Spiderman")
+heroPower.push(2)
+
+allusers.push({name: "h", isActive: true})
+
+export {}
