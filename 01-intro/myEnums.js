a947251e93c8f8bf9388b24d4e641ced5b1947d5
Nadatari <dariosting@gmail.com>
2022-12-12 22:18:29 +0100

    Finished lesson with enums

---
+++ 01-intro/myEnums.js
@@ -0,0 +1,13 @@
+"use strict";
+/* const AISLE = 0;
+const MIDDLE = 1;
+const WINDOW = 2; */
+exports.__esModule = true;
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice["MIDDLE"] = "middle";
+    SeatChoice["WINDOW"] = "window";
+    SeatChoice[SeatChoice["FOURTH"] = 44] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeat = SeatChoice.AISLE;
---
+++ 01-intro/myEnums.ts
@@ -0,0 +1,19 @@
+
+/* const AISLE = 0;
+const MIDDLE = 1;
+const WINDOW = 2; */
+
+enum SeatChoice {
+    AISLE = "aisle", //if value is 10, next one is incremented by one
+    MIDDLE = "middle", //11 if value is string, you have to assign value to others also
+    WINDOW = "window", //12
+    FOURTH = 44
+}
+
+const hcSeat = SeatChoice.AISLE
+
+
+
+
+
+export {}
