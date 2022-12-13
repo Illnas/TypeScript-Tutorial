b173016c52f187ab53f72ecdce547d896e90b073
Nadatari <dariosting@gmail.com>
2022-12-13 17:48:27 +0100

    Finished second lesson on interfaces

--- 01-intro/myInterface.js
+++ 01-intro/myInterface.js
@@ -4,11 +4,13 @@ var nada = {
     dbId: 123,
     email: "h@h.com",
     userId: 1234,
+    role: "admin",
     startTrial: function () {
         return "Trial Started";
     },
     getCoupon: function (name, off) {
         return 10;
-    }
+    },
+    githubToken: "github"
 };
 nada.email = "dwadkandja";
--- 01-intro/myInterface.ts
+++ 01-intro/myInterface.ts
@@ -9,16 +9,34 @@ interface User {
    getCoupon(couponname: string, value: number): number
 }
 
-const nada: User = {
+interface User {
+    //this is reopening of interface
+    githubToken: string
+}
+
+interface Another {
+
+}
+
+
+interface Admin extends User, Another {
+    role: "admin" | "ta" | 'learner'
+
+}
+
+
+const nada: Admin = {
     dbId: 123,
     email: "h@h.com",
     userId: 1234,
+    role: "admin",
     startTrial: () => {
         return "Trial Started"
     },
     getCoupon: (name: "nada", off: 23) => {
         return 10
-    }
+    },
+    githubToken: "github"
 }
 
 nada.email = "dwadkandja"
