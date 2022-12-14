31430fd31ea3eb872123836d1c17ded2ef8f2218
Nadatari <dariosting@gmail.com>
2022-12-14 16:33:36 +0100

    Finished lesson with classes and private, public keywords

--- 02-purets/dist/index.js
+++ 02-purets/dist/index.js
@@ -1,3 +1,23 @@
 "use strict";
 console.log("TS is here!");
 console.log("TS is here again!");
+/* class User {
+    public email: string; //everything that you dont mark, is automatically public
+    private name: string; //also can use # instead of private
+     readonly city: string = "" //private marks something not usable outside of its scope
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+} */
+class User {
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this.city = ""; //private marks something not usable outside of its scope
+        this.email = email;
+        this.name = name;
+    }
+}
+const nada = new User("h@nada.com", "Nada", '1234');
--- 02-purets/src/index.ts
+++ 02-purets/src/index.ts
@@ -2,3 +2,30 @@
 console.log("TS is here!")
 
 console.log("TS is here again!")
+
+
+/* class User {
+    public email: string; //everything that you dont mark, is automatically public
+    private name: string; //also can use # instead of private
+     readonly city: string = "" //private marks something not usable outside of its scope
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+} */
+
+
+class User {
+     readonly city: string = "" //private marks something not usable outside of its scope
+    constructor(
+        public email: string,
+        public name: string,
+        private userId: string
+        ){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const nada = new User("h@nada.com", "Nada", '1234')
+
