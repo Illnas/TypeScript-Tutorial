4df2b843068e53415785f62f62e3f3199f6b7c9f
Nadatari <dariosting@gmail.com>
2022-12-16 21:33:55 +0100

    Finished lesson about narrowing

---
+++ 02-purets/dist/narrowing.js
@@ -0,0 +1,33 @@
+"use strict";
+// problem arises when you check for array number []number
+function detectTypes(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("Please provide ID");
+        return;
+    }
+    id.toLowerCase();
+}
+//example from TS documentation, DON'T DO THIS!!! 
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
---
+++ 02-purets/src/narrowing.ts
@@ -0,0 +1,53 @@
+
+// problem arises when you check for array number []number
+function detectTypes(val: number | string) {
+    if( typeof val === "string") {
+        return val.toLowerCase()
+    }
+
+    return val + 3
+}
+
+
+function provideId(id: string | null) {
+    if(!id) {
+        console.log("Please provide ID")
+        return
+    }
+
+    id.toLowerCase()
+}
+
+//example from TS documentation, DON'T DO THIS!!! 
+function printAll(strs: string | string[] | null) {
+  
+    if(strs) {
+        if(typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s)
+            }
+        } else if (typeof strs === "string") {
+            console.log(strs)
+        }
+    }
+}
+
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+
+function isAdminAccount(account: User | Admin) {
+    if("isAdmin" in account) {
+        return account.isAdmin
+    }
+
+
+}
